5b26dba6bca2247fdebf496953df7aebab231d9b
LeraSidarava <126901447+LeraSidarava@users.noreply.github.com>
2023-04-19 21:41:19 +0200

    Add files via upload
    Lecture_004 add functionality of task 2

---
+++ AnimalsAndHerbal.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+public abstract class Alive
+{
+    public abstract string SpeciesName();
+}
+
+public class Animal : Alive
+{
+    private string name;
+
+    public Animal(string name)
+    {
+        this.name = name;
+    }
+
+    public override string SpeciesName()
+    {
+        return "Animal";
+    }
+
+    public virtual string GetName()
+    {
+        return name;
+    }
+}
+
+public abstract class Carnivore : Animal
+{
+    protected string speciesName;
+
+    public Carnivore(string name, string speciesName) : base(name)
+    {
+        this.speciesName = speciesName;
+    }
+
+    public override string SpeciesName()
+    {
+        return speciesName;
+    }
+
+    public abstract void Eat(Animal animal);
+}
+
+public abstract class Herbivore : Animal
+{
+    public Herbivore(string name) : base(name)
+    {
+    }
+
+    public override string SpeciesName()
+    {
+        return "Herbivore";
+    }
+
+    public abstract void Eat(Herbal herbal);
+}
+
+public abstract class Omnivore : Animal
+{
+    public Omnivore(string name) : base(name)
+    {
+    }
+
+    public override string SpeciesName()
+    {
+        return "Omnivore";
+    }
+
+    public abstract void Eat(Alive alive);
+}
+
+public class Wolf : Carnivore
+{
+    public Wolf(string name) : base(name, "Carnivore")
+    {
+    }
+
+    public override void Eat(Animal animal)
+    {
+        Console.WriteLine("{0} is eating {1}", GetName(), animal.GetName());
+    }
+}
+
+public class Rabbit : Herbivore
+{
+    public Rabbit(string name) : base(name)
+    {
+    }
+
+    public override string SpeciesName()
+    {
+        return "Herbivore";
+    }
+
+    public override void Eat(Herbal herbal)
+    {
+        Console.WriteLine("{0} is eating {1}", GetName(), herbal.GetName());
+    }
+}
+
+
+public class Bear : Omnivore
+{
+    public Bear(string name) : base(name)
+    {
+    }
+
+    public override string SpeciesName()
+    {
+        return "Omnivore";
+    }
+
+    public override void Eat(Alive alive)
+    {
+        if (alive is Animal)
+        {
+            Console.WriteLine("{0} is eating {1}", GetName(), ((Animal)alive).GetName());
+        }
+        else if (alive is Herbal)
+        {
+            Console.WriteLine("{0} is eating {1}", GetName(), ((Herbal)alive).GetName());
+        }
+    }
+}
+
+public class Rose : Herbal
+{
+    public Rose(string name) : base(name)
+    {
+    }
+
+    public override string SpeciesName()
+    {
+        return "Herbal";
+    }
+}
+
+public class Grass : Herbal
+{
+    public Grass(string name) : base(name)
+    {
+    }
+
+    public override string SpeciesName()
+    {
+        return "Herbal";
+    }
+}
+
+public abstract class Herbal : Alive
+{
+    private string name;
+
+    public Herbal(string name)
+    {
+        this.name = name;
+    }
+
+    public override string SpeciesName()
+    {
+        return "Herbal";
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Wolf myWolf = new Wolf("Wolf");
+        Rabbit myRabbit = new Rabbit("Rabbit");
+        Bear myBear = new Bear("Bear");
+        Grass myGrass = new Grass("Grass");
+        Rose myRose = new Rose("Rose");
+
+        Console.WriteLine(myWolf.GetName());
+        Console.WriteLine("{0} is {1}.", myWolf, myWolf.SpeciesName());
+
+        Console.WriteLine(myRabbit.GetName());
+        Console.WriteLine("{0} is {1}.", myRabbit, myRabbit.SpeciesName());
+
+        Console.WriteLine(myBear.GetName());
+        Console.WriteLine("{0} is {1}.", myBear,myBear.SpeciesName());
+
+        Console.WriteLine(myGrass.GetName());
+        Console.WriteLine("{0} is {1}.", myGrass,myGrass.SpeciesName());
+        
+        Console.WriteLine(myRose.GetName()); 
+        Console.WriteLine("{0} is {1}.",myRose,myRose.SpeciesName());
+
+        myWolf.Eat(myRabbit);
+        myRabbit.Eat(myGrass);
+        myBear.Eat(myRabbit);
+        myBear.Eat(myGrass);
+        myBear.Eat(myRose);
+    }
+}
